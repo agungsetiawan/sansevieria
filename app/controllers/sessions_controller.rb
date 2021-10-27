2a425e2447690d89d746ea53729fda62b5cf2d08
Hauril Maulida Nisfari <hauril.nisfari@gmail.com>
2021-10-27 09:43:05 +0700

    create migration account, user, useraccount, create simple session

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,32 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    username = params[:username]
+    password = params[:password_digest]
+
+    user = User.find_by(username: username)
+    if user
+      if user.authenticate(password)
+        session[:user_id] = user.id
+        redirect_to '/welcome', notice: "Logged in!"
+      else
+        redirect_to '/login', notice: 'Username atau password salah'
+      end
+    else
+      redirect_to '/login', notice: 'Username atau password salah'
+    end
+  end
+
+  def login
+  end
+
+  def welcome
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/welcome', notice: "Logged out!"
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  has_secure_password 
+
+  validates :name, presence: true, length: {minimum: 3}
+  validates :username, presence: true, uniqueness: true
+  validates :password, confirmation: { case_sensitive: true }
+  validates :password_confirmation, presence: true
+end
